ddeb45ab00415a93e985ee87a678cff6efca1029
Trinmar Boado <timi.boado@gmail.com>
2022-06-27 11:04:43 +0800

    Initial commit

---
+++ frontend/src/pages/TodoApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Hello {{ name }}</h3>
+    <input type="text" v-model="name">
+    <h3>Hello2 {{ human.name }}</h3>
+    <input type="text" v-model="human.name">
+  </div>
+</template>
+<script setup>
+import { ref, reactive } from 'vue'
+
+const name = ref('Jake Zyrus')
+const human = reactive({
+  name: 'Xander Ford',
+  age: 21
+})
+</script>
