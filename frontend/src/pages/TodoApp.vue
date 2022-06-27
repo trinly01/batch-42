cb82b79159560ee98e7fe44253fa4e9e8024e405
Trinmar Boado <timi.boado@gmail.com>
2022-06-27 13:06:07 +0800

    Todo MVC

--- frontend/src/pages/TodoApp.vue
+++ frontend/src/pages/TodoApp.vue
@@ -1,17 +1,47 @@
 <template>
   <div>
-    <h3>Hello {{ name }}</h3>
-    <input type="text" v-model="name">
-    <h3>Hello2 {{ human.name }}</h3>
-    <input type="text" v-model="human.name">
+    <h3>workaborad todos</h3>
+    <input v-model="data.task" type="text" placeholder="what needs to be done?" @keyup.enter="addTask">
+    <ul>
+      <li v-for="t in data.todos" :key="t.id">
+        <input type="checkbox" :value="t.isDone" :checked="t.isDone" @change="changeIsDone(t)">
+        <span
+          :style="{'text-decoration': t.isDone ? 'line-through': ''}"
+        >
+          {{ t.desc }}
+        </span>
+        <button>x</button>
+      </li>
+    </ul>
+    {{ data.todos.filter(t => !t.isDone).length }} items left
   </div>
 </template>
 <script setup>
-import { ref, reactive } from 'vue'
+import { reactive } from 'vue'
 
-const name = ref('Jake Zyrus')
-const human = reactive({
-  name: 'Xander Ford',
-  age: 21
+const data = reactive({
+  task: '',
+  todos: [
+    {
+      _id: Date.now(),
+      desc: 'asd',
+      isDone: true
+    }
+  ]
 })
+
+function changeIsDone (t) {
+  console.log('asdasd')
+  t.isDone = !t.isDone
+}
+
+const addTask = () => {
+  data.todos.unshift({
+    _id: Date.now(),
+    desc: data.task,
+    isDone: false
+  })
+
+  data.task = ''
+}
 </script>
