897d1e87ca73c0f1eebc8d84682ab43a3cb0ac2b
Trinmar Boado <timi.boado@gmail.com>
2022-06-28 15:22:38 +0800

    array map

--- frontend/src/pages/TodoApp.vue
+++ frontend/src/pages/TodoApp.vue
@@ -13,21 +13,19 @@
     <pie-chart :donut="true" :data="[['active', numbers.active], ['completed', numbers.completed]]"></pie-chart>
     <div class="q-pa-md q-gutter-sm">
       <div class="md-row">
-        <q-input class="col" v-model="data.task" label="todo" value="" placeholder="what needs to be done" @keyup.enter="addTask" />
+        <q-input class="col" v-model="data.task" label="todo" value="" placeholder="what needs to be done"
+          @keyup.enter="addTask" />
         <q-btn label="add" />
       </div>
       <div class="row">
-        <q-tabs
-          v-model="data.tab"
-          class="text-teal col"
-        >
-          <q-tab name="all" icon="mail" label="All" >
+        <q-tabs v-model="data.tab" class="text-teal col">
+          <q-tab name="all" icon="mail" label="All">
             <q-badge v-if="numbers.all" color="grey" floating>{{ numbers.all }}</q-badge>
           </q-tab>
-          <q-tab name="active" icon="alarm" label="Active" >
+          <q-tab name="active" icon="alarm" label="Active">
             <q-badge v-if="numbers.active" color="red" floating>{{ numbers.active }}</q-badge>
           </q-tab>
-          <q-tab name="completed" icon="movie" label="Completed" >
+          <q-tab name="completed" icon="movie" label="Completed">
             <q-badge v-if="numbers.completed" color="green" floating>{{ numbers.completed }}</q-badge>
           </q-tab>
         </q-tabs>
@@ -38,27 +36,23 @@
           <q-item-section avatar>
             <q-checkbox v-model="t.isDone" />
           </q-item-section>
-          <q-item-section
-            @dblclick="data.editing = i"
-            v-if="data.editing !== i "
-            :style="{
-              'text-decoration': t.isDone ? 'line-through' : ''
-            }"
-            :class="{
-              'text-grey': t.isDone
-            }"
-          >
+          <q-item-section @dblclick="data.editing = i" v-if="data.editing !== i" :style="{
+            'text-decoration': t.isDone ? 'line-through' : ''
+          }" :class="{
+  'text-grey': t.isDone
+}">
             {{ t.desc }}
           </q-item-section>
-          <q-input v-else-if="editing === i" v-model="t.desc" @keyup.enter="data.editing = -1" @keyup.esc="data.editing = -1"></q-input>
+          <q-input v-else-if="editing === i" v-model="t.desc" @keyup.enter="data.editing = -1"
+            @keyup.esc="data.editing = -1"></q-input>
           <q-item-section side>
             <q-btn flat round icon="delete" size="sm" color="red" />
           </q-item-section>
         </q-item>
       </q-list>
-    <div>
-      {{ itemsLeft }} items left
-    </div>
+      <div>
+        {{ itemsLeft }} items left
+      </div>
     </div>
     <cj-steven v-model="data.food" :name="data.name" gender="male" :age="data.age" @ageAdded="ageData => data.age = ageData + 1" />
   </div>
@@ -79,9 +73,38 @@ const data = reactive({
       _id: Date.now(),
       desc: 'asd',
       isDone: true
+    },
+    {
+      _id: Date.now() + 123123,
+      desc: 'xcv',
+      isDone: true
     }
   ]
 })
+
+function printTodos () {
+  console.log('original', data.todos)
+  const arrMap = data.todos.map(row => [row._id, row.desc, row.isDone])
+  console.log('arrMap', arrMap)
+  const dd = {
+    // Print To dos
+    // Columns: ID, Description, Status
+    content: [
+      {
+        style: 'tableExample',
+        table: {
+          body: [
+            ['ID', 'Description', 'Status'],
+            ...arrMap
+          ]
+        }
+      }
+    ]
+  }
+  console.log('table', dd.content[0].table.body)
+  return dd
+}
+printTodos()
 
 const itemsLeft = computed(() => data.todos.filter(t => !t.isDone).length)
 
